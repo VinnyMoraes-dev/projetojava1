c7d0652d18dfc2cb5596d0a71b287cad9195d284
ViniDSA <56524332+ViniDSA@users.noreply.github.com>
2022-04-05 00:28:39 -0300

    Alteração no comentário

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -2,10 +2,10 @@ package cursojava.executavel;
 
 public class PrimeiraClasseJava {
 	
-	/*Main é um método auto executável em Java*/
+	/*-- Main é um método auto executável em Java --*/
 	public static void main(String[] args) {
 		
-		/*Escreve texto no console  */
+		/*-- Escreve texto no console --*/
 		System.out.println("My First Program in Java. Hello World!");
 		System.out.println("Java é orientado a objeto");
 	}
