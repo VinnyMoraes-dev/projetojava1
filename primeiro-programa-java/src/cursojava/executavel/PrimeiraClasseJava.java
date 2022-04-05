dbd3780e16028c30566ec8ed083dc09d4b54859c
ViniDSA <56524332+ViniDSA@users.noreply.github.com>
2022-04-05 00:08:43 -0300

    Criação do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,13 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/*Main é um método auto executável em Java*/
+	public static void main(String[] args) {
+		
+		/*Escreve texto no console*/
+		System.out.println("My First Program in Java. Hello World!");
+		System.out.println("Java é orientado a objeto");
+	}
+
+}
